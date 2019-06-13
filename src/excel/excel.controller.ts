ff5e3ab98e40b04d157fc3c5a369fdcaf9907a8f
shdwu <250276837@qq.com>
2019-06-13 21:56:22 +0800

    导出完成

--- src/excel/excel.controller.ts
+++ src/excel/excel.controller.ts
@@ -2,19 +2,19 @@ import * as express from 'express';
 import xlsx from 'node-xlsx';
 import Controller from '../interfaces/controller.interfaces';
 import poorModel from '../poor/poor.model';
-import townModel from '../town/town.model';
-import villageModel from '../village/village.model';
 import Poor from '../poor/poor.interface';
 import Town from '../town/town.interface';
 import * as multer from 'multer';
 import cache from '../utils/cache';
+import fs = require('fs');
+import stream = require('stream');
+import path = require('path');
+import shell = require('shelljs');
 
 class ExcelController implements Controller {
   public path = '/excel';
   public router = express.Router();
   private poor = poorModel;
-  private town = townModel;
-  private village = villageModel;
   private upload: multer.Instance;
 
   constructor() {
@@ -25,6 +25,56 @@ class ExcelController implements Controller {
   private initializeRouters() {
     this.router.post(`${this.path}/parse`, this.upload.single('file'), this.parseExcel);
     this.router.post(`${this.path}/enter`, this.enterExcel);
+    this.router.post(`${this.path}/export`, this.exportExcel);
+  }
+
+  private exportExcel = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const search: any = req.query;
+    if ( req.user.town ) {
+      search.town =  req.user.town || search.town;
+    }
+    if (req.query.name) {
+      const nameReg = new RegExp(req.query.name, 'i');
+      search.name = nameReg;
+    }
+    this.poor.find(search).populate('town').populate('village').then(poors => {
+      const rows = [['乡镇', '村社', '组', '姓名', '户编号',	'与户主关系',	'身份证号码',	'联系电话',	'就业情况',	'无劳动能力状况',	'就业',
+        '就业类型',	'具体工种',	'就业地',	'工资收入',	'是否有培训意愿',	'培训项目意愿',	'未就业原因',	'姓名',	'单位及职务',	'联系电话'
+      ]];
+      if (poors && poors.length > 0) {
+        poors.forEach(p => { rows.push(this.poor2arr(p)); });
+      }
+      const buffer = xlsx.build([
+        {name: 'Sheet1', data: rows}
+      ]);
+      res.end(Buffer.from(buffer).toString('base64'));
+    });
+  }
+
+  private poor2arr(poor: Poor) {
+    const arr = [];
+    arr.push(poor.town.name || '');
+    arr.push(poor.village.name || '');
+    arr.push(poor.team || '');
+    arr.push(poor.name || '');
+    arr.push(poor.cell.cellCode || '');
+    arr.push(poor.cell.relationship || '');
+    arr.push(poor.userCode || '');
+    arr.push(poor.phone || '');
+    arr.push(poor.jobState || '');
+    arr.push(poor.state || '');
+    arr.push(poor.isJob || '');
+    arr.push(poor.jobType || '');
+    arr.push(poor.workType || '');
+    arr.push(poor.jobAdd || '');
+    arr.push(poor.salary || '');
+    arr.push(poor.train || '');
+    arr.push(poor.trainItem || '');
+    arr.push(poor.noJobSeason || '');
+    arr.push(poor.helpPerson.name || '');
+    arr.push(poor.helpPerson.position || '');
+    arr.push(poor.helpPerson.phone || '');
+    return arr;
   }
 
   private initializeUpload() {

a2b6a361e0848ee000246322f022d8e26f10ddcd
yy <250276837@qq.com>
2019-10-21 20:06:03 +0800

    test

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import HttpException from '../exceptions/HttpException';
 
 function authMiddleware(request: Request, response: Response, next: NextFunction) {
+  process.stdout.write(request.path);
   // After successful login, redirect back to the intended page
   if (!request.user &&
     request.path !== '/auth/login' &&
